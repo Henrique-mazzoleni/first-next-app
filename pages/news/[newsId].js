41c94fd04ed0f55411332428f0c7f4823a07a63b
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-07-03 12:15:27 +0200

    link routing

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router"
 export default function () {
     const router = useRouter()
 
-    const newsId = String(router.query.newsId).replace('-', ' ')
+    const newsId = String(router.query.newsId).replace(/-/g, ' ')
 
     return <h1>The Details Page for {newsId}</h1>
 };
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,3 +1,14 @@
-const NewsPage = () => <h1>The News Page</h1>
+import { Fragment } from "react";
+import Link from "next/link";
 
-export default NewsPage;    
+export default function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <div className="list">
+        <Link href="/news/yoyoma">The Celist?</Link>
+        <Link href="/news/pigman">Cmon George, let's be realistic</Link>
+      </div>
+    </Fragment>
+  );
+}
